505a486e9b53a8e93325a315e484131cc357d167
RomTurino <romturino@gmail.com>
2021-07-08 20:09:51 +0300

    The process of creating the blocks now avoids the place for the future bomberman

--- bomberman.py
+++ bomberman.py
@@ -6,6 +6,8 @@ import arcade
 # задаем ширину, высоту и заголовок окна
 SCREEN_WIDTH = 660
 SCREEN_HEIGHT = 660
+ROW_COUNT = 11
+COLUMN_COUNT = 11
 CELL_WIDTH = 60
 CELL_HEIGHT = 60
 SCREEN_TITLE = "Bomberman"
@@ -20,6 +22,15 @@ class ExplodableBlock(arcade.Sprite):
     def __init__(self):
         super().__init__('Blocks/ExplodableBlock.png')
 
+class Bomberman(arcade.AnimatedTimeSprite):
+    def __init__(self):
+        super().__init__(0.5)
+        self.texture = arcade.load_texture('Bomberman/Front/Bman_F_f00.png')
+        for i in range(8):
+            self.textures.append(arcade.load_texture(f'Bomberman/Front/Bman_F_f0{i}.png'))
+        self.center_y = SCREEN_HEIGHT/ROW_COUNT - CELL_HEIGHT/2
+        self.center_x = SCREEN_WIDTH/COLUMN_COUNT - CELL_WIDTH/2
+
 
 class OurGame(arcade.Window):
     # Конструктор окна
@@ -28,11 +39,13 @@ class OurGame(arcade.Window):
         self.bg = arcade.load_texture('Blocks/BackgroundTile.png')
         self.blocks = arcade.SpriteList()
         self.occupied_places = []
+        self.bomberman = Bomberman()
+        self.walk = False
 
     # начальные значения
     def setup(self):
-        for x in range(11):
-            for y in range(11):
+        for x in range(ROW_COUNT):
+            for y in range(COLUMN_COUNT):
                 if x % 2 == 1 and y % 2 == 1:
                     solid_block = SolidBlock()
                     solid_block.center_x = x * 60 + CELL_WIDTH / 2
@@ -41,7 +54,7 @@ class OurGame(arcade.Window):
                     self.occupied_places.append((solid_block.center_x, solid_block.center_y))
                 if random.randint(1, 2) == 1:
                     print(f"Ну, икс здесь {x}, а игрек {y}")
-                    if (x>=2 and y>0) or (x>0 and y>=2) or (x>10 and y<10) or (x<10 and y>10):#условие должно выбросить из зоны появления блоков две площадки для главных героев
+                    if not(x==0 and y<=2) and not( y == 0 and x<=2) and not(y==10  and x>=9) and not(x==10 and y>=9):#условие должно выбросить из зоны появления блоков две площадки для главных героев
                         explodable_block = ExplodableBlock()
                         explodable_block.center_x = x * 60 + CELL_WIDTH / 2
                         explodable_block.center_y = y * 60 + CELL_HEIGHT / 2
@@ -57,10 +70,13 @@ class OurGame(arcade.Window):
                 arcade.draw_texture_rectangle(x * 60 + CELL_WIDTH / 2, y * 60 + CELL_HEIGHT / 2, CELL_WIDTH,
                                               CELL_HEIGHT, self.bg)
         self.blocks.draw()
+        self.bomberman.draw()
 
     # игровая логика
     def update(self, delta_time):
-        pass
+        self.bomberman.update()
+        if self.walk:
+            self.bomberman.update_animation()
 
     # нажатие на клавишу
     def on_key_press(self, symbol: int, modifiers: int):
